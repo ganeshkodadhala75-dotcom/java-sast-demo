7b81529601713b3b5cb64a24c0da1e684ed143fc
ganeshkodadhala75-dotcom <ganeshkodadhala75@gmail.com>
2026-07-24 10:36:38 +0530

    Create SQLInjection.java
    Add SQLInjection.java

---
+++ src/main/java/com/example/SQLInjection.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class SQLInjection {
+
+    public static void main(String[] args) {
+
+        String username = "admin";
+
+        try {
+
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/test",
+                    "root",
+                    "password");
+
+            Statement stmt = con.createStatement();
+
+            String query =
+                    "SELECT * FROM users WHERE username='"
+                            + username + "'";
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                System.out.println(rs.getString("username"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
